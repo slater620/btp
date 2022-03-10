05e87fc2ae4a9c0d0dfea23888caab752a2bf0a6
you <ffomekong@gmail.com>
2022-03-10 14:03:33 +0000

    essai 12

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const config = require('./common/config/env.config.js');
 
 const express = require('express');
 const app = express();
+const cors=require('cors');
+
 const taches= require('./taches/models/taches.model');
 const AuthorizationRouter = require('./authorization/routes.config');
 const ProjectRouter = require('./projets/routes.config');
@@ -15,18 +17,20 @@ const notif=require('./notifications/Jobs/jobs');
 // const TypeRouter=require('./types/routes.config');
 const CommandeRouter= require('./commandes/routes.config');
 
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-//     res.header('Access-Control-Expose-Headers', 'Content-Length');
-//     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-//     if (req.method === 'OPTIONS') {
-//         return res.sendStatus(200);
-//     } else {
-//         return next();
-//     }
-// });
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    } else {
+        return next();
+    }
+});
+
 
 app.use(express.json());
 AuthorizationRouter.routesConfig(app);
